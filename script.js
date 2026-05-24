a9e48a332bd4a779ed6ccecc094e449ee4212f78
sahbabiilover92 <257049988+sahbabiilover92@users.noreply.github.com>
2026-05-24 10:42:38 -0400

    Created function that takes two numbers and an operator and logs the solution

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const subtractFunction = (x,y) => x-y;
 const multiplyFunction = (x,y) => x*y;
 const divideFunction = (x,y) => x/y;
 
+//variables for numbers in operation and operator.
+const numOne = "";
+const numTwo = "";
+const operatorVar = "";
+
+const operate = function(numA,numB,operator){
+
+    switch (operator) {
+        case "add":
+            console.log(addFunction(numA,numB));
+            break;
+        case "subtract":
+            console.log(subtractFunction(numA,numB));
+            break;
+        case "multiply":
+            console.log(multiplyFunction(numA,numB));
+            break;
+        case "divide":
+            console.log(divideFunction(numA,numB));
+            break;
+    }
+};
