e937a772b05e1cff0baf0ee731dee9876f14b7b4
sahbabiilover92 <257049988+sahbabiilover92@users.noreply.github.com>
2026-05-24 16:56:13 -0400

    Created a click event that updates the display with the number clicked

--- script.js
+++ script.js
@@ -5,7 +5,26 @@ const multiplyFunction = (x,y) => x*y;
 const divideFunction = (x,y) => x/y;
 
 //variables for numbers in operation and operator.
-const numOne = "";
+const one = document.getElementById('1');
+const two = document.getElementById('2');
+const three = document.getElementById('3');
+const four = document.getElementById('4');
+const five = document.getElementById('5');
+const six = document.getElementById('6');
+const seven = document.getElementById('7');
+const eight = document.getElementById('8');
+const nine = document.getElementById('9');
+const zero = document.getElementById('0');
+const display = document.getElementById('display');
+let numElements = [one,two,three,four,five,six,seven,eight,nine,zero];
+
+const numOne =
+    numElements.map((element)=>{
+        element.addEventListener('click',()=> {
+            display.innerText = `${display.innerText}` + `${element.textContent}`;
+            });
+    });
+
 const numTwo = "";
 const operatorVar = "";
 
