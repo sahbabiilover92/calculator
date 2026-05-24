dcf4d7a879446fed647a8cb2626689e2d50462a3
sahbabiilover92 <257049988+sahbabiilover92@users.noreply.github.com>
2026-05-24 10:16:54 -0400

    Created basic functions for add, subtract, multiply, and divide

---
+++ script.js
@@ -0,0 +1,6 @@
+//functions for add, subtract, multiply, and divide.
+const addFunction = (x,y) => x+y;
+const subtractFunction = (x,y) => x-y;
+const multiplyFunction = (x,y) => x*y;
+const divideFunction = (x,y) => x/y;
+
